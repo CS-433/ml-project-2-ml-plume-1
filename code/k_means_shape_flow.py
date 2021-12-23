2b464e3a053c112f79a6245d923d002a73d8f17a
Danepfl <danielgonczy@gmail.com>
2021-12-23 01:26:32 +0100

    Implementation of labels in k-means for Part 2

--- code/k_means_shape_flow.py
+++ code/k_means_shape_flow.py
@@ -8,47 +8,46 @@ from matplotlib import pyplot as plt
 # from helper_clustering import import_data, visualize, obtain_clusters, info_clusters
 
 # We import the data (already normalized over a year of samples)
-pars_path = r"C:\Users\Dan\Desktop\Hourly_Discharge.csv"
+data_path = r"C:\Users\Dan\Desktop\Hourly_Discharge.csv"
 labels_path = r"C:\Users\Dan\Desktop\Labels_Clustering.csv"
-pars = pd.read_csv(pars_path)
-cols = pars.columns
+data = pd.read_csv(data_path)
+cols = data.columns
 for col in cols:
-    pars[col] = pars[col].astype(float)
-data = pars
-data['Clusters'] = 0
+    data[col] = data[col].astype(float)
+data['Labels kmeans'] = -1
+data['Labels gmm'] = -1
 labels = pd.read_csv(labels_path)
 
 # Merging datasets based on Unix Time
 for index, row in labels.iterrows():
     name = labels.at[index,'Img name']
     unix_tmp = int(name[19:29])
-    pos = round((unix_tmp-pars.at[0,'Unix time'])/3600)
-    pars.at[pos,'Clusters'] = labels.at[index,'Cluster #']
+    pos = round((unix_tmp-data.at[0,'Unix time'])/3600)
+    data.at[pos,'Labels kmeans'] = labels.at[index,'Clusters kmeans']
+    data.at[pos,'Labels gmm'] = labels.at[index,'Clusters gmm']
 
 # Removing unnecessary lines in dataset
 data = data.dropna()
 for index, row in data.iterrows():
-    if data.at[index,'Clusters'] == 0 :
+    if data.at[index,'Labels kmeans'] == -1 :
         data = data.drop([index])
 
 # Separation of labels and data
 data = data.reset_index()
 features = data[['Flow rate','Temperature','Wind direction','Wind speed','Wind speed squared']]
-true_labels = data[['Clusters']]
-tmp = true_labels.values
-true_labels = tmp.ravel()
+
+labels_kmeans = data[['Labels kmeans']]
+labels_gmm = data[['Labels gmm']]
+tmp1 = labels_kmeans.values
+tmp2 = labels_gmm.values
+labels_kmeans = tmp1.ravel()
+labels_gmm = tmp2.ravel()
 
 # We classify the data with k-means
-np.random.seed(1)
-kmeans = KMeans(n_clusters=3, init='random', n_init=100)
+np.random.seed(0)
+kmeans = KMeans(n_clusters=4, init='random', n_init=100)
 kmeans.fit(features)
 
-# We classify the data with DBSCAN
-dbscan = DBSCAN(eps=0.3, min_samples=5)
-dbscan.fit(features)
-
-# Comparison with DBSCAN
-kmeans_silhouette = silhouette_score(features, kmeans.labels_).round(2)
-dbscan_silhouette = silhouette_score(features, dbscan.labels_).round(2)
-ari_kmeans = adjusted_rand_score(true_labels, kmeans.labels_)
-ari_dbscan = adjusted_rand_score(true_labels, dbscan.labels_)
+# Comparison K-means/DBSCAN and with labeling K-means/GMM
+ari_kmeans1 = adjusted_rand_score(labels_kmeans, kmeans.labels_).round(4)
+ari_gmm1 = adjusted_rand_score(labels_gmm, kmeans.labels_).round(4)
