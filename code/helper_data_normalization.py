2f8bf764e89aa08b74923bab1301b689b669ac77
Paula Rescala <paula.rescala@epfl.ch>
2021-12-23 07:00:23 -0600

    cleaning code

--- code/helper_data_normalization.py
+++ code/helper_data_normalization.py
@@ -1,3 +1,6 @@
+'''Helper file for data normalization step.'''
+
+#imports
 import os
 import cv2
 import numpy as np
@@ -5,15 +8,19 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from helper_pca import *
 
+
 def get_data_grayscale(file_path):
+    '''Extract the data from the images in grayscale.'''
+    data = [] # initialize data matrix
+    filenames = os.listdir(file_path) # get filenames from path
+    filenames_new = [] #initialize array that will contain the filenames that were not empty
     
-    data = []
-    filenames = os.listdir(file_path)
-    filenames_new = []
-    
+    # loop through each file
     for file_idx, filename in enumerate(filenames):
+        #extract image in grayscale
         img = cv2.imread(os.path.join(file_path,filename), cv2.IMREAD_GRAYSCALE)
-        if img is None:
+        #skip to next image if current one is empty otherwise append to data
+        if img is None: 
             pass
         else:
             data.append(img)
@@ -24,17 +31,21 @@ def get_data_grayscale(file_path):
     
     
 def get_data_color(file_path):
+    ''' Extract the data from the images in HSV.'''
     
+    # intialize arrays
     h_vals = []
     s_vals = []
     v_vals = []
     filenames = os.listdir(file_path)
     
+    # loop through each file
     for filename in filenames:
-        img = cv2.imread(os.path.join(file_path,filename)) 
+        img = cv2.imread(os.path.join(file_path,filename)) # read image in color
         if img is None:
             pass
         else:
+            # if image is not empty separate into hsv values
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
             h,s,v = cv2.split(hsv)
             h_vals.append(h)
@@ -45,9 +56,12 @@ def get_data_color(file_path):
 
 
 def normalize_brightness(file_path, method = 'histogram_flattening'):
-    
+    '''Normalize the images by a specified method.'''
+    # get h,s,v values
     h_vals, s_vals, v_vals = get_data_color(file_path)
-    results = []
+    results = [] # intialize results array
+    
+    # loop through each image and apply normalization method
     for v in v_vals:
         if method == 'histogram_flattening':
             result = cv2.equalizeHist(v)
@@ -57,6 +71,7 @@ def normalize_brightness(file_path, method = 'histogram_flattening'):
             result = clahe.apply(v)
             results.append(result)
         else:
+            # data standardization
             s = 32
             m = 128
             v = (v-np.mean(v)) / np.std(v) * s + m
@@ -64,6 +79,7 @@ def normalize_brightness(file_path, method = 'histogram_flattening'):
             results.append(result)
 
     data=[]
+    # convert the results back into a grayscale image and append to data
     for result, h, s in zip(results, h_vals, s_vals):
         hsv = cv2.merge((h,s,result))
         rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
